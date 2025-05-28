35b65b0c0d684a2ff1fd0db23a39f80713582fe1
MrN0B0DY23 <imraan648@gmail.com>
2025-05-28 21:56:42 +0200

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log('hello world')
 let text_inp = document.getElementById('todo_input') 
 let add_to_list = document.getElementById("add_btn")
 let display = document.getElementById("display")
+let list_items=[]
 add_to_list.addEventListener("click",()=>{
     // console.log('hello world')
     // display.innerHTML +="TO DO ITEM"
@@ -10,19 +11,30 @@ add_to_list.addEventListener("click",()=>{
 
 function create(){
     if(text_inp.value.length>0){
+        list_items.push(text_inp.value)
  read()       
     }
 }
 function read (){
-    display.innerHTML +=`<li onclick="del(Event)">${text_inp.value}</li>`
+   display.innerHTML=""
+    for (let index = 0; index < list_items.length; index++) {
+    display.innerHTML +=`<li onclick="del(event)"oncontextmenu="update(event)">${list_items[index]}</li>`
+    }
         text_inp.value =""
 } 
-function update(){
-    console.log("should update or something like that")
+function update(event){
+    if(text_inp.value.length>0){
+        event.preventDefault()
+        let update_item = text_inp.value
+        let index = list_items.indexOf(event.target.innerHTML)
+        list_item = list_items.splice(index, 1, update_item);
     read()
+    }
+    
 }
 function del (event){
-    console.log(event)
-
-
+    console.log(event.target.innerHTML)
+    let del_item = event.target.innerHTML
+    list_items = list_items.filter((task) => task= del_item)
+    read()
 }
